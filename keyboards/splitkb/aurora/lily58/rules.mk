3d520ff0355be3fbaf9364272cbeefd1512af80a
Petar Metodiev <petar.metodiev@gmail.com>
2025-03-28 10:28:00 +0200

    bring back rules.mk

---
+++ keyboards/splitkb/aurora/lily58/rules.mk
@@ -0,0 +1,6 @@
+# You should have received a copy of the GNU General Public License
+ # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ CONVERT_TO=liatris
+ CAPS_WORD_ENABLE = yes
+ DEFAULT_FOLDER = splitkb/aurora/lily58/rev1
